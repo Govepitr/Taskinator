0c153c647c69721587002d0094e3390278d62dbf
Dan Arbelo <govepitr@gmail.com>
2020-12-31 08:06:47 -0700

    prevented browser default from event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
 
-var buttonEl = document.querySelector("#save-task");
-var tasksToDoEl = document.querySelector("#tasks-to-do");
+var formEl = document.querySelector("#task-form"); 
+var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
 //Task creation (must be placed BEFORE the event listener to avoid an undefined error)
-var createTaskHandler = function() {
-  var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
-  tasksToDoEl.appendChild(listItemEl);
-}
+//Included preventDefault event in function to avoid browser refreshing after sepcific event occurred
+var createTaskHandler = function(event) { 
+  event.preventDefault(); 
 
-//Button creation w/clickable event
-buttonEl.addEventListener("click", createTaskHandler);
+  var listItemEl = document.createElement("li"); 
+  listItemEl.className = "task-item"; 
+  listItemEl.textContent = "This is a new task."; 
+  tasksToDoEl.appendChild(listItemEl); 
+  }; 
 
-
+  formEl.addEventListener("submit", createTaskHandler);
